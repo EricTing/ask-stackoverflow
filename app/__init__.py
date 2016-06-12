dd3f1e51d218254338c3e512815b7446c77c7d25
Ubuntu <ubuntu@ip-172-31-6-242.ec2.internal>
2016-06-12 06:31:14 +0000

    prototype prediction page
    	modified:   app/__init__.py
    	modified:   app/templates/index.html
    	new file:   app/templates/predict.html
    	modified:   app/views.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 from app import views
+
+from sklearn.externals import joblib
+
+clf = joblib.load(
+    "/home/ubuntu/Workspace/WhenStackStopsOverFlow/logistic_regression.pkl")
+reg = joblib.load(
+    "/home/ubuntu/Workspace/WhenStackStopsOverFlow/linear_regression.pkl")
--- app/views.py
+++ app/views.py
@@ -1,9 +1,35 @@
+from flask import request
 from flask import render_template
 from app import app
+
+import pandas as pd
+from sklearn.externals import joblib
+
+clf = joblib.load(
+    "/home/ubuntu/Workspace/WhenStackStopsOverFlow/logistic_regression.2015-11-01.pkl")
+reg = joblib.load(
+    "/home/ubuntu/Workspace/WhenStackStopsOverFlow/linear_regression.2016-01-01.pkl")
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
-    return render_template("index.html", title='Home', user=user)
+    return render_template("index.html")
+
+
+@app.route('/predict')
+def predict():
+    title = request.args.get('title')
+    question = request.args.get('question')
+    tags = request.args.get('tags')
+
+    df = pd.DataFrame({"title": [title],
+                       "tags": [tags],
+                       "paragraphs": [question],
+                       "hasCodes": [1]})
+    proba = clf.predict_proba(df)[0, 1]
+    time = reg.predict(df)[0]
+
+    return render_template("predict.html",
+                           response={"proba": proba,
+                                     "time": time})
